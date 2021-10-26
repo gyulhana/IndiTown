6f1059594a4fac93c97a8d858268e4bcb85f9e50
kimin3004 <kimin3004@gmail.com>
2021-10-26 18:36:24 +0900

    Feat: 수정 Button 컴포넌트 styled

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,5 +1,6 @@
-import styled from '@emotion/styled'
 import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+import { darken } from 'polished'
 
 const Buttons = styled.button`
   border: none;
@@ -9,29 +10,27 @@ const Buttons = styled.button`
   font-size: 1rem;
   font-weight: 500;
   padding: 0.5rem 1.4rem;
+  background-color: ${({ primary }) => (primary ? '#F6B545' : '#333333')};
+  color: ${({ primary }) => (primary ? '#333333' : '#FAFAFA')};
+  filter: ${({ primary }) =>
+    primary
+      ? 'drop-shadow(0 8px 16px rgba(229, 125, 8, 0.12))'
+      : 'drop-shadow(0 8px 16px rgba(51, 51, 51, 0.12))'};
 
   &:hover {
-    background-color: #111;
+    background-color: ${({ primary }) =>
+      primary ? darken(0.1, '#F6B545') : darken(0.1, '#333333')};
   }
 
   &:active {
-    background-color: #222;
+    background-color: ${({ primary }) =>
+      primary ? darken(0.13, '#F6B545') : darken(0.13, '#333333')};
   }
 `
 
 const Button = ({ children, large, size, color, primary = true, ...props }) => {
-  const buttonStyle = {
-    ...props.style,
-    backgroundColor: primary === true ? '#F6B545' : '#333333',
-    color: primary === true ? '#333333' : '#FAFAFA',
-    filter:
-      primary === true
-        ? 'drop-shadow(0 8px 16px rgba(229, 125, 8, 0.12))'
-        : 'drop-shadow(0 8px 16px rgba(51, 51, 51, 0.12))',
-  }
-
   return (
-    <Buttons style={buttonStyle} {...props}>
+    <Buttons primary={primary} {...props}>
       {children}
     </Buttons>
   )
