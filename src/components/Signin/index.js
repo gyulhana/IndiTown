89bbc54b5a35b5bac84a9ade4d501b3512dec998
JunHyeongB <aowlr0513@gmail.com>
2021-10-27 14:48:04 +0900

    Feat: 로그인 컴포넌트 추가, 버튼 컴포넌트 적용

---
+++ src/components/Signin/index.js
@@ -0,0 +1,108 @@
+import styled from '@emotion/styled'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
+import Input from '../Form'
+import Button from '../Button'
+
+const FormContainer = styled.form`
+  padding: 1.25rem 3.75rem;
+`
+
+const ButtonContainer = styled.div`
+  text-align: center;
+`
+
+const Invalid = styled.div`
+  height: 1rem;
+  color: #dc3545;
+  font-size: 0.75rem;
+  margin: 0.5rem 0 6.25rem;
+  text-indent: 0.75rem;
+`
+
+const invalidErrorMessage = {
+  required: '아이디 또는 비밀번호를 입력해주세요.',
+  notCorrect: '아이디 또는 비밀번호가 올바르지 않습니다.',
+}
+
+const Signin = () => {
+  const API_ENDPOINT = 'http://13.209.30.200'
+
+  const formik = useFormik({
+    initialValues: {
+      userEmail: '',
+      userPassword: '',
+    },
+    validationSchema: Yup.object({
+      userEmail: Yup.string()
+        .email(invalidErrorMessage.notCorrect)
+        .required(invalidErrorMessage.required),
+      userPassword: Yup.string().required(invalidErrorMessage.required),
+    }),
+    onSubmit: async (values) => {
+      const userInfo = {
+        email: values.userEmail,
+        password: values.userPassword,
+      }
+
+      const result = await fetch(`${API_ENDPOINT}/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(userInfo),
+      })
+
+      if (!result.ok) {
+        return formik.setErrors({ userEmail: invalidErrorMessage.notCorrect })
+      }
+
+      console.log(result)
+    },
+  })
+
+  return (
+    <FormContainer onSubmit={formik.handleSubmit}>
+      <Input
+        large
+        type="email"
+        name="userEmail"
+        placeholder="ID"
+        style={{ marginBottom: '1.25rem' }}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.userEmail}
+      />
+      <Input
+        large
+        type="password"
+        name="userPassword"
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        value={formik.values.userPassword}
+      />
+      {(formik.touched.userEmail && formik.errors.userEmail) ||
+      (formik.touched.userPassword && formik.errors.userPassword) ? (
+        <Invalid>{formik.errors.userEmail}</Invalid>
+      ) : (
+        <Invalid />
+      )}
+      <ButtonContainer>
+        <Button
+          style={{
+            backgroundColor: 'initial',
+            margin: '0 auto',
+          }}
+          type="button"
+        >
+          회원가입
+        </Button>
+        <Button style={{ fontSize: '1.25rem', width: '100%' }} type="submit">
+          로그인
+        </Button>
+      </ButtonContainer>
+    </FormContainer>
+  )
+}
+
+export default Signin
