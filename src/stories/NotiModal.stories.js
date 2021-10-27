b98bfa25c942b494740e78c05d76627222a0678a
gyulhana <matearz@korea.ac.kr>
2021-10-27 16:28:28 +0900

    feat: add initial notification modal wo/feature

---
+++ src/stories/NotiModal.stories.js
@@ -0,0 +1,11 @@
+import NotiModal from '../components/NotiModal'
+
+export default {
+  title: 'Component/NotiModal',
+  component: NotiModal,
+  argTypes: {},
+}
+
+export const Default = (args) => {
+  return <NotiModal {...args} />
+}
