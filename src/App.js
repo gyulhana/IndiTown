bbb5ce4e2834cb8ec11c7b4223bd69a0d4d395b5
JunHyeongB <aowlr0513@gmail.com>
2021-10-29 00:02:28 +0900

    Feat: 적용 React router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { Route, Switch } from 'react-router'
+import LocationProvider from './contexts/LocationProvider'
+import { CheckLocation, MainPage, SigninPage, SignupPage } from './pages'
+
 function App() {
-  return <div />
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <MainPage />
+        </Route>
+        <Route path="/location" exact>
+          <LocationProvider>
+            <CheckLocation />
+          </LocationProvider>
+        </Route>
+        <Route path="/signup">
+          <LocationProvider>
+            <SignupPage />
+          </LocationProvider>
+        </Route>
+        <Route path="/signin" component={SigninPage}></Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App
