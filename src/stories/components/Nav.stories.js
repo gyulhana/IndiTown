c930fd263b4f92c0032550ec030965a5e2890e1d
gyulhana <matearz@korea.ac.kr>
2021-10-31 14:34:44 +0900

    Feat: 추가 프로필 네비게이션 스토리북 추가

--- src/stories/components/Nav.stories.js
+++ src/stories/components/Nav.stories.js
@@ -13,3 +13,32 @@ export const Default = () => {
     </Nav>
   )
 }
+
+export const MyProfileNav = () => {
+  return (
+    <Nav style={{ gridTemplateColumns: 'repeat(3, 1fr)' }}>
+      <Nav.Item title="모집내역" index="writes">
+        모집
+      </Nav.Item>
+      <Nav.Item title="참여내역" index="participant">
+        참여
+      </Nav.Item>
+      <Nav.Item title="관심내역" index="likes">
+        관심
+      </Nav.Item>
+    </Nav>
+  )
+}
+
+export const OtherProfileNav = () => {
+  return (
+    <Nav style={{ gridTemplateColumns: 'repeat(2, 1fr)' }}>
+      <Nav.Item title="모집내역" index="writes">
+        모집
+      </Nav.Item>
+      <Nav.Item title="참여내역" index="participant">
+        참여
+      </Nav.Item>
+    </Nav>
+  )
+}
