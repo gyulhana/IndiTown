a221dc31029462d0168f3bbf195ce683d74be9ac
gyulhana <matearz@korea.ac.kr>
2021-11-03 00:20:19 +0900

    Style: TextArea 전송 아이콘 크기 수정

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -72,8 +72,8 @@ const TextArea = ({
       <StyledButton>
         <svg
           width="22"
-          height="20"
-          viewBox="0 0 22 20"
+          height="18"
+          viewBox="0 0 22 18"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
           style={{ marginRight: '0.8rem' }}
